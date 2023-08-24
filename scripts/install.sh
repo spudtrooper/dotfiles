c3d236b75cba05981d17d15b243f5d68d0e3a431
Jeff Palm <jeff@jeffpalm.com>
2023-08-24 07:51:47 -0400

    update Thu Aug 24 07:51:47 EDT 2023

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,3 +4,19 @@ for f in files/*; do
   dest="$HOME/.$(basename "$f")"
   cp $f $dest
 done
+
+# Outputs the most likely file that is your main bash RC file.
+function rcfile() {
+  if [[ -f "$HOME/.zprofile" ]]; then
+    echo "$HOME/.zprofile"
+  elif [[ "$SHELL" == *"/zsh"* ]]; then
+    echo "$HOME/.zshrc"
+  else
+    echo "$HOME/.bashrc"
+  fi
+}
+
+echo "Remember to run the following command:"
+echo
+echo "source $(rcfile)"
+echo
