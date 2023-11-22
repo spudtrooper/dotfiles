a20d77a694b919635be9f529a35a0c5cfbea7f5d
Jeff Palm <jeff@jeffpalm.com>
2023-11-22 08:57:59 -0500

    update Wed Nov 22 08:57:59 EST 2023

--- src/bash_functions.d/nmap_all.sh
+++ src/bash_functions.d/nmap_all.sh
@@ -6,19 +6,37 @@ set -e
 
 arp_scan=$(arp-scan --localnet 2>&1)
 echo "$arp_scan"
-ips_str=$(echo $arp_scan | \
-  grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | \
-  tr '\n' ' ' | \
-  sort)
+
+ips=()
+manufacturers=()
+
+while read -r line; do
+    ip=$(echo "$line" | awk '{print $1}')
+    manufacturer=$(echo "$line" | awk '{for(i=3;i<=NF;++i) printf $i " "; print ""}')
+    ips+=("$ip")
+    manufacturers+=("$manufacturer")
+done <<< "$(echo "$arp_scan" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}.*$')"
 
 tmp_dir=$(mktemp -d)
+echo "saving to $tmp_dir ..."
 
-echo $ips_str | \
-  xargs -I {} -n 1 -P 30 sh -c \
-    "nmap -A --osscan-guess -T4 -v -sV {} | tee $tmp_dir/{}"
+for ip in "${ips[@]}"; do
+    nmap -A --osscan-guess -T4 -v -sV "$ip" | tee "$tmp_dir/$ip" &
+done
+
+wait
 
 for f in $tmp_dir/*; do
-  ip=$(basename $f)
-  echo "--- $ip"
-  grep /tcp "$f"
+  ip=$(basename "$f")
+
+  for i in "${!ips[@]}"; do
+    if [ "${ips[$i]}" = "$ip" ]; then
+      manufacturer="${manufacturers[$i]}"
+      break
+    fi
+  done
+
+  echo
+  echo "--- $ip ($manufacturer) ---"
+  awk '/PORT/,/Nmap scan report for/ {if (!/Nmap scan report for/ && !/PORT/) print}' "$f"
 done
